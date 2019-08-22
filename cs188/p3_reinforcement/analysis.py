e2e6afaeb77a623aa2016a951698e31e6d8df022
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2019-08-23 00:24:51 +0200

    Solve Project 3, Question 2

--- cs188/p3_reinforcement/analysis.py
+++ cs188/p3_reinforcement/analysis.py
@@ -21,8 +21,9 @@
 
 def question2():
     answerDiscount = 0.9
-    answerNoise = 0.2
+    answerNoise = 0.0
     return answerDiscount, answerNoise
+
 
 def question3a():
     answerDiscount = None
@@ -31,12 +32,14 @@ def question3a():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3b():
     answerDiscount = None
     answerNoise = None
     answerLivingReward = None
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3c():
     answerDiscount = None
@@ -45,12 +48,14 @@ def question3c():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3d():
     answerDiscount = None
     answerNoise = None
     answerLivingReward = None
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3e():
     answerDiscount = None
@@ -59,15 +64,18 @@ def question3e():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question8():
     answerEpsilon = None
     answerLearningRate = None
     return answerEpsilon, answerLearningRate
     # If not possible, return 'NOT POSSIBLE'
 
+
 if __name__ == '__main__':
     print('Answers to analysis questions:')
     import analysis
+
     for q in [q for q in dir(analysis) if q.startswith('question')]:
         response = getattr(analysis, q)()
         print('  Question %s:\t%s' % (q, str(response)))
